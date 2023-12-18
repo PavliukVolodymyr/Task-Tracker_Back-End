97fa63cb0ffa9e8bf89d4a9e9f35ea6250201dbb
Roman <rhoryachko@gmail.com>
2023-12-18 20:46:49 +0200

    Added /admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import EmailVerification, UserProfile
 
-# Register your models here.
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'is_verified', 'verification_token', 'email']
+    search_fields = ['user__username', 'email']
+
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'photo']
+    search_fields = ['user__username']
